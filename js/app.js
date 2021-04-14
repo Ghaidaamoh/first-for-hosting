d6cb4184ef9fb93da122c3291f40b9b8d4d8346b
ghaidaamoh <>
2021-04-14 11:57:38 +0000

    create new js

--- js/app.js
+++ js/app.js
@@ -33,15 +33,15 @@ if (salary ===1000 && user=='ghaidaa')
 else{
   console.log ('false');
 }
-var userage = prompt('please enter your age');
+var userage = prompt('please enter your age( from 15-20 , from 21-60) ');
  console.log ('user age' + userage);
 
- if (userage <= 20 && userage>=15 )
+ if (userage == 'from 15-20')
  {
    var image = '<img src ="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.com%2FHello-Teen-Cake-Topper-Decorations%2Fdp%2FB086KTX3LP&psig=AOvVaw2TrCtfWtsDFLiHFqA-9B9S&ust=1618486294593000&source=images&cd=vfe&ved=2ahUKEwjPwsm20f3vAhX8gv0HHb_MCt0QjRx6BAgAEAc" alt= "teenager"/>';
    document.write(image);
  }
- else if (userage >= 20 && userage <= 60 )
+ else if (userage == 'from 21-60' )
  {
    var image2 = '<img src ="img/adults.jpg" alt= "adults"/>';
    document.write(image2);
